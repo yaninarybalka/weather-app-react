2a1ae7bd5d96537149fbf9a833e7c0b6b43d03cc
yaninarybalka <rybalka.yanina@gmail.com>
2022-07-09 16:02:39 +0300

    renamed App files to Main, added to index.css structure

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import "./Main.css";
+
+export default function Main() {
+  return (
+    <div className="A">
+      <header className="App-header">
+        <p>Hello!</p>
+      </header>
+    </div>
+  );
+}
